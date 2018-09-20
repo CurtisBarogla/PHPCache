b5dd7ad23da54fc6c551aa11b0bc1d84749e4ce0
CurtisBarogla <curtis_barogla@outlook.fr>
2018-09-20 09:04:40 +0200

    Remove construct parameters which was useless

--- src/NullCache.php
+++ src/NullCache.php
@@ -13,7 +13,6 @@ declare(strict_types = 1);
 namespace Ness\Component\Cache;
 
 use Ness\Component\Cache\Adapter\NullCacheAdapter;
-use Psr\Log\LoggerInterface;
 
 /**
  * Null cache
@@ -26,15 +25,8 @@ class NullCache extends AbstractCache
     
     /**
      * Null cache
-     *
-     * @param int|null|\DateInterval $defaultTtl
-     *   Default ttl to apply to the cache pool and the cache. Must be compatible
-     * @param string|null $namespace
-     *   Namespace of the cache. If setted to null, will register cache values into global namespace
-     * @param LoggerInterface|null $logger
-     *   If a logger is setted, will log errors when a cache value cannot be setted
      */
-    public function __construct($defaultTtl = null, ?string $namespace = null, ?LoggerInterface $logger = null)
+    public function __construct()
     {
         $this->adapter = new NullCacheAdapter();
         parent::__construct(null, null, null);
